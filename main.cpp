e24014a95948a8158cd67b8ee1751adce0190ba5
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-10-29 21:21:25 -0600

    Init project

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "./lib/reader.hpp"
+#include "./lib/substrings.hpp"
+#include "./lib/palindromes.hpp"
+#include "./lib/longest_substring.hpp"
+
+
+int main (int argc, char *argv[]) {
+  std::vector<std::string> transmissions;
+  std::vector<std::string> maliciousCodes;
+
+  for (int i = 1; i < 3; ++i)
+    transmissions.push_back(reader("./transmission" + i + ".txt"))
+  
+  for (int i = 1; i < 4; ++i)
+    maliciousCodes.push_back(reader("./mcode1.txt"));
+  
+  /* ToDo:
+    Part 1 Logs
+    Part 2 Logs
+    Part 3 Logs
+  */
+  
+  return 0;
+}
