4b6eda8e0ba837df5602d3cac830079abf36145a
Iker Garcia <A01782767@tec.mx>
2023-11-02 21:57:06 -0600

    Added Comments

--- lib/substrings.hpp
+++ lib/substrings.hpp
@@ -13,24 +13,30 @@
 #include <vector>
 
 namespace p1 {
+  // Function to preprocess a pattern string and return a vector of integers
   std::vector<int> preprocessing(const std::string &pattern) {
+    // Find the length of the pattern
       int n = pattern.length();
 
+      // Create a vector with the size of the pattern length
       std::vector<int> V(n);
+      // Initialize two pointers for processing the pattern
       int j = 0;
       int i = 1;
+      // Base condition for the first character of the pattern
       V[0] = 0;
 
+      // Loop through the pattern to create the preprocessing table
       while (i < n) {
-          if (pattern[i] == pattern[j]) {
+          if (pattern[i] == pattern[j]) { // If the characters match, update the preprocessing table
               V[i] = j + 1;
               i++;
               j++;
           } else {
-              if (j == 0) {
+              if (j == 0) { // If no match and j is at the beginning, reset the table entry
                   V[i] = 0;
                   i++;
-              } else {
+              } else { // If no match, update j to the last known match
                   j = V[j - 1];
               }
           }
@@ -38,35 +44,37 @@ namespace p1 {
 
       return V;
   }
-
+  // Function to check if a pattern string is a substring of a text string
   bool isSubstring(const std::string &text, const std::string &pattern) {
+    // Find the lengths of the text and the pattern
     int n = text.length();
     int m = pattern.length();
 
     std::vector<int> V = preprocessing(pattern);
+    // Initialize pointers for both text and pattern
     int pos = 0;
     int j = 0;
     int i = 0;
-
+    // Loop through the text to find the pattern
     while (i < n) {
-      if (text[i] == pattern[j]) {
+      if (text[i] == pattern[j]) { // If the current characters match, increment pointers
         i++;
         j++;
 
-        if (j == m) {
+        if (j == m) { // If we have reached the end of the pattern, return true
           return true;
         }
-      } else {
-        if (j == 0) {
+      } else { // If no match and j is at the beginning, increment i
+        if (j == 0) { 
           i++;
           pos = i;
-        } else {
+        } else { // If no match, update i and j based on the preprocessing table
           pos = i - V[j - 1];
           j = V[j - 1];
         }
       }
     }
-
+    // If the pattern was not found in the text, return false
     return false;
   }
 }
