598d63bc5f7dd49bed104eeb987345ff3af20cff
Iker Garcia <A01782767@tec.mx>
2023-11-02 20:54:23 -0600

    Added Some comments to longest

--- lib/longest_substring.hpp
+++ lib/longest_substring.hpp
@@ -17,13 +17,18 @@
 #include <string>
 #include <iostream>
 
+// Enclose everything in a namespace to prevent name collisions
 namespace p3 {
+  // Using an optional type for the result.
+  // This means the result can either hold a value of type pair<int, int> or no value at all.
   typedef std::optional<std::pair<int, int>> Result;
 
+  // Returns the longest substring between s1 and s2
   Result longest_substring(const std::string &s1, const std::string &s2) {
     int n = s1.length();
     int m = s2.length();
 
+    // 2D memoization table
     int memo[n + 1][m + 1];
     int max = 0;
     int end = -1;
@@ -38,22 +43,27 @@ namespace p3 {
     // Fill the DP matrix
     for (int i = 1; i < n + 1; i++) {
       for (int j = 1; j < m + 1; j++) {
+        // If characters match, consider it as part of the substring
         if (s1[i - 1] == s2[j - 1]) {
           memo[i][j] = memo[i - 1][j - 1] + 1;
+          // If current substring is longer than max, update max and end
           if (memo[i][j] > max) {
             max = memo[i][j];
             end = i - 1;
           }
         } 
         else {
+          // If characters do not match, reset current cell to 0
           memo[i][j] = 0;
         }
       }
     }
     
+    // If no common substring was found, return an empty optional
     if (end == -1) 
       return {};
 
+    // Construct result using the max length and the end position
     std::pair<int, int> res = std::make_pair(end - max + 1, end);
     return res;
   }
